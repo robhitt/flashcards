3f276f1e53d0cc118783f66e80ca2c0f0745a63a
Rob Hitt <robhitt@gmail.com>
2018-03-01 23:37:10 -0500

    Update JSON and build out test page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
 window.addEventListener('load', app)
 
 function app() {
-    
+  const url = "https://robhitt.github.io/flashcards/card-data.json";
+  fetch(url, {method: "GET"})
+    .then(response => response.json())
+    .then( (response) => {
+      renderPage(response);
+    })
+}
+
+function renderPage(response) {
+  const quizData = response.quizData;
+  const cardContainer = document.querySelector(".container");
+  let html;
+
+  quizData.forEach( data => {
+    html = `
+      <div>${data.question}</div>
+      <div>${data.answer}</div>
+      <div>==============</div>
+    `;
+
+    cardContainer.insertAdjacentHTML('beforeend', html);
+  });
   
 }
